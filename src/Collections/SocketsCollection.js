ffad241f55c79fe06f251a1afc23406f6811e29e
LanTenDragon <pantheonia1@gmail.com>
2020-07-08 01:43:31 +0800

    Use map and filter functions instead of foreach

--- src/Collections/SocketsCollection.js
+++ src/Collections/SocketsCollection.js
@@ -2,26 +2,23 @@ const m = require('mithril')
 const SocketPieces = require('../models/SocketPiece')
 
 const SocketsCollection = {
-  currentGroup: 1,
+  currentGroup: '0',
   set: function (i) {
     SocketsCollection.currentGroup = i
   },
   oninit: SocketPieces.SocketData.state.fetch,
   view: function () {
-    let display = false
-    SocketPieces.SocketData.state.list.map(item =>
-      item.groups.map(item2 => {
-        if (item2 === SocketsCollection.currentGroup) {
-          display = true
-        }
-      })
-    )
-
-    if (display) {
-      return SocketPieces.SocketData.state.list.map(item =>
-        m(SocketPieces.Piece, { _id: item._id })
-      )
-    } else return m('p', 'Currently no sockets to display')
+    if (SocketsCollection.currentGroup === '0') {
+      console.log('hit')
+      return SocketPieces.SocketData.state.list
+        .filter(item => item.groups.length === 0)
+        .map(item => m(SocketPieces.Piece, { _id: item._id }))
+    } else {
+      console.log('hit2')
+      return SocketPieces.SocketData.state.list
+        .filter(item => item.groups.includes(SocketsCollection.currentGroup))
+        .map(item => m(SocketPieces.Piece, { _id: item._id }))
+    }
   }
 }
 
