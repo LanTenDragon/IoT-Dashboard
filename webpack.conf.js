456303dc6f3038590e561198ee2b31016f624aef
LanTenDragon <pantheonia1@gmail.com>
2020-07-08 01:34:02 +0800

    Update webpack to use environment variable

--- webpack.conf.js
+++ webpack.conf.js
@@ -1,11 +1,21 @@
 const path = require('path')
+const DotEnv = require('dotenv-webpack')
 
-module.exports = {
-  mode: 'production',
-  entry: './src/index.js',
-  watch: true,
-  output: {
-    path: path.resolve(__dirname, 'public/js'),
-    filename: 'index.js'
+module.exports = (env, argv) => {
+  const envPath = env.ENVIRONMENT ? `.env.${env.ENVIRONMENT}` : '.env.dev'
+
+  return {
+    mode: 'production',
+    entry: './src/index.js',
+    watch: true,
+    output: {
+      path: path.resolve(__dirname, 'public/js'),
+      filename: 'index.js'
+    },
+    plugins: [
+      new DotEnv({
+        path: envPath
+      })
+    ]
   }
 }
