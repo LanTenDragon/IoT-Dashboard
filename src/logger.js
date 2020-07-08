a0aacef8790723ef05cbb259d74535a8c7fda960
LanTenDragon <pantheonia1@gmail.com>
2020-07-09 01:48:39 +0800

    Change development to dev

--- src/logger.js
+++ src/logger.js
@@ -1,5 +1,5 @@
 function Logger (message) {
-  if (process.env.NODE_ENV === 'development') console.log(message)
+  if (process.env.NODE_ENV === 'dev') console.log(message)
 }
 
 module.exports = Logger
