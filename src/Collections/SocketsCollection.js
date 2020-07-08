406272232810e2f0c32f95bc4267e5312438b9e2
LanTenDragon <pantheonia1@gmail.com>
2020-07-09 00:53:11 +0800

    Remove logging lines

--- src/Collections/SocketsCollection.js
+++ src/Collections/SocketsCollection.js
@@ -9,12 +9,10 @@ const SocketsCollection = {
   oninit: SocketPieces.SocketData.state.fetch,
   view: function () {
     if (SocketsCollection.currentGroup === '0') {
-      console.log('hit')
       return SocketPieces.SocketData.state.list
         .filter(item => item.groups.length === 0)
         .map(item => m(SocketPieces.Piece, { _id: item._id }))
     } else {
-      console.log('hit2')
       return SocketPieces.SocketData.state.list
         .filter(item => item.groups.includes(SocketsCollection.currentGroup))
         .map(item => m(SocketPieces.Piece, { _id: item._id }))
