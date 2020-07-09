5fd5ae2630e5e79945c3f67b4be971dde022d0ed
LanTenDragon <pantheonia1@gmail.com>
2020-07-09 14:31:52 +0800

    Update webpack config

--- webpack.conf.js
+++ webpack.conf.js
@@ -3,11 +3,12 @@ const DotEnv = require('dotenv-webpack')
 
 module.exports = (env, argv) => {
   const envPath = env.ENVIRONMENT ? `.env.${env.ENVIRONMENT}` : '.env.dev'
+  const shouldWatch = env.ENVIRONMENT === 'dev'
 
   return {
     mode: 'production',
     entry: './src/index.js',
-    watch: true,
+    watch: shouldWatch,
     output: {
       path: path.resolve(__dirname, 'public/js'),
       filename: 'index.js'
