45c1447761756d6697b728603b700d7b17fa92fd
LanTenDragon <pantheonia1@gmail.com>
2020-07-26 21:38:35 +0800

    Add weekly power usage

--- src/pages/PowerUsage.js
+++ src/pages/PowerUsage.js
@@ -5,14 +5,25 @@ const url = process.env.URL
 
 const UsageData = {
   data: {
-    list: [],
+    dailyPowerList: [],
+    weeklyPowerList: [],
     fetch: () => {
       m.request({
-        method: 'GET',
-        url: url + '/statistics/' + localStorage.getItem('userid') + '/power'
+        method: 'POST',
+        url: url + '/statistics/' + localStorage.getItem('userid') + '/power',
+        body: { days: 1 }
       })
         .then((res) => {
-          UsageData.data.list = res
+          UsageData.data.dailyPowerList = res
+        })
+
+      m.request({
+        method: 'POST',
+        url: url + '/statistics/' + localStorage.getItem('userid') + '/power',
+        body: { days: 7 }
+      })
+        .then((res) => {
+          UsageData.data.weeklyPowerList = res
         })
     }
   }
@@ -34,35 +45,82 @@ const PowerUsage = {
               )
             )
           ),
-          m('table', { class: 'w3-table w3-striped w3-bordered w3-border w3-hoverable w3-white' },
-            m('tbody',
+          [
+            m('div', { class: 'w3-bar w3-grey' },
               [
-                m('tr',
-                  [
-                    m('td',
-                      m('b', 'Socket Name')
-                    ),
-                    m('td',
-                      m('b', 'Watts')
-                    )
-                  ]
+                m('button', { class: 'w3-bar-item w3-button w3-padding w3-blue tabButton', id: 'buttonDaily', onclick: () => { openTab('Daily') } },
+                  'Past 24 Hours'
                 ),
-                UsageData.data.list.map(item => {
-                  return m('tr',
+                m('button', { class: 'w3-bar-item w3-button w3-padding tabButton', id: 'buttonWeekly', onclick: () => { openTab('Weekly') } },
+                  'Past 7 Days'
+                )
+              ]
+            ),
+            m('div', { class: 'tab', id: 'Daily' },
+              [
+                m('table', { class: 'w3-table w3-striped w3-bordered w3-border w3-hoverable w3-white' },
+                  m('tbody',
                     [
-                      m('td',
-                        item.name
+                      m('tr',
+                        [
+                          m('td',
+                            m('b', 'Socket Name')
+                          ),
+                          m('td',
+                            m('b', 'Watts')
+                          )
+                        ]
                       ),
-                      m('td',
-                        item.total
-                      )
+                      UsageData.data.dailyPowerList.map(item => {
+                        return m('tr',
+                          [
+                            m('td',
+                              item.name
+                            ),
+                            m('td',
+                              item.total
+                            )
+                          ]
+                        )
+                      })
                     ]
                   )
-                })
+                )
+              ]
+            ),
+            m('div', { class: 'tab', id: 'Weekly', style: { display: 'none' } },
+              [
+                m('table', { class: 'w3-table w3-striped w3-bordered w3-border w3-hoverable w3-white' },
+                  m('tbody',
+                    [
+                      m('tr',
+                        [
+                          m('td',
+                            m('b', 'Socket Name')
+                          ),
+                          m('td',
+                            m('b', 'Watts')
+                          )
+                        ]
+                      ),
+                      UsageData.data.weeklyPowerList.map(item => {
+                        return m('tr',
+                          [
+                            m('td',
+                              item.name
+                            ),
+                            m('td',
+                              item.total
+                            )
+                          ]
+                        )
+                      })
+                    ]
+                  )
+                )
               ]
             )
-          ),
-          m('br')
+          ]
         ]
       )
     )
